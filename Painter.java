fc4f8a065ae6056e8b8a062614aece8de503d73b
benjaminc8190 <114585884+benjaminc8190@users.noreply.github.com>
2025-12-04 10:45:16 -0500

    tried paintComponent

--- Painter.java
+++ Painter.java
@@ -1,4 +1,4 @@
-import java.swing.*
+import javax.swing.*
 import java.awt.*
 
 public Painter extends JFrame{
@@ -14,9 +14,16 @@ public Painter extends JFrame{
     this.add(canvas, BorderLayout.CENTER);
     this.add(panel, BorderLayout.SOUTH);   
   }
+
+  protected void paintComponent(Graphics g){
+    super.paintComponent(g);
+    g.setColor(Color.BLUE);
+    g.drawString("Hello, World!", 60, 70);
+  }
+  
   public static void main(String[] args){
     Painter paint = new Painter();
-    paint.setTitle("ControlCircle1");
+    paint.setTitle("Painter");
     paint.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     paint.setSize(500, 500);
     paint.setVisible(true);
